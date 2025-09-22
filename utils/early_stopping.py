6de8328c0e5f9efb5812a48abe54f102ed6802cb
wow-rao <158815107+wow-rao@users.noreply.github.com>
2025-09-22 15:55:55 +0530

    Update early_stopping.py

--- utils/early_stopping.py
+++ utils/early_stopping.py
@@ -1,19 +1,23 @@
 class EarlyStopping:
-    def __init__(self, patience=5, delta=0, verbose=False):
+    def __init__(self, patience=5, delta=0, verbose=False, restore_weights=True):
         self.patience = patience
         self.delta = delta
         self.verbose = verbose
         self.best_loss = None
         self.no_improvement_count = 0
         self.stop_training = False
+        self.restore_weights = restore_weights
+        self.weights = None
     
-    def check_early_stop(self, val_loss):
+    def check_early_stop(self, val_loss, model):
         if self.best_loss is None or val_loss < self.best_loss - self.delta:
             self.best_loss = val_loss
             self.no_improvement_count = 0
+            self.weights = model.state_dict(),
         else:
             self.no_improvement_count += 1
             if self.no_improvement_count >= self.patience:
                 self.stop_training = True
                 if self.verbose:
                     print("Stopping early as no improvement has been observed.")
+                    
